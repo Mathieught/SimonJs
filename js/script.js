c50dc3ac2ad3be3270959d5c689b70f17af00f7a
mathieught <mathieu.ght@gmail.com>
2019-11-23 21:50:27 +0100

    ^^

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ let playerScore = []; // ordre du joueur
 let roundCount = 0; // count lvl
 let playerCount = 0; // count lvl
 // let turn = 1;// savoir a qui est le tour
-let match; // check bonne reponse ou non
 let simonTurn = true; // savoir quand le joueur ou simon joue
 let PlayerTurn = false;
 let interval;// interval fonction flash lumiere
@@ -21,6 +20,7 @@ const $btnb = document.getElementById('btnblue');
 const $btng = document.getElementById('btngreen');
 const $btnr = document.getElementById('btnred');
 const $btny = document.getElementById('btnyellow');
+
 let $0 = document.getElementsByClassName('.0');
 let $1 = document.getElementsByClassName('.1');
 let $2 = document.getElementsByClassName('.2');
@@ -40,8 +40,7 @@ const $btntest = document.getElementById('btnblue');
 // onclick start btn
 $btnstart.addEventListener('click', () => {
     on = true;
-    roundCount = 1
-    ;
+    roundCount = 1;
     clearTimeout(interval);
     $scorebx1.innerHTML = roundCount;
     $scorebx2.innerHTML = playerCount;
@@ -77,49 +76,15 @@ function changeTurn(){
         if(simonTurn = false){
             PlayerTurn = true ;
             clearTimeout(interval);
+            match();
         };
+        if(simonTurn == true && PlayerTurn == false){
+            roundCount++;
+            play();
+        }
         console.log(PlayerTurn);
         console.log('marche', simonTurn)
-        if(on === true && simonTurn === false && PlayerTurn === false){
-            $btnb.addEventListener('click', () => {
-                $0 = 0;
-                if($0 === 0){
-                    playerScore.push($0);
-                    console.log(playerScore);
-                    $btnb.classList.add('activated');
-                    setTimeout(function (){
-                        $btnb.classList.remove('activated');
-                    },1000);
-                };
-            });
-            $btng.addEventListener('click', () => {
-                $1 = 1;
-                if($1 === 1){
-                    $btng.classList.add('activated');
-                    setTimeout(function (){
-                        $btng.classList.remove('activated');
-                    },1000);
-                };
-            });
-            $btnr.addEventListener('click', () => {
-                $2 = 2;
-                if($2 === 2){
-                    $btnr.classList.add('activated');
-                    setTimeout(function (){
-                        $btnr.classList.remove('activated');
-                    },1000);
-                };
-            });
-            $btny.addEventListener('click', () => {
-                $3 = 3;
-                if($3 === 3){
-                    $btny.classList.add('activated');
-                    setTimeout(function (){
-                        $btny.classList.remove('activated');
-                    },1000);
-                }
-            });
-        };
+        Player();
     };
 }
 function autorun(){
@@ -127,22 +92,81 @@ function autorun(){
     changeTurn();
     if(on === true && simonTurn === true){
         x = roundCount;
-        playerCount++; 
+        playerCount++;
         $scorebx1.innerHTML = roundCount;
         $scorebx2.innerHTML = playerCount;
         console.log(playerCount);
-    for (i = 0; i < x; i++) {
-        console.log('marche', simonTurn)
+        for (i = 0; i < x; i++) {
         simon = [];
         simon.push(Math.floor((Math.random() * 4)));
-        // simonOrder.push(simon);
+        console.log('marche', simonTurn)
+        simonOrder.push(simon);
         console.log(simon);
         console.log(simonOrder);
         flash();
     }
 }
 };
-
+function Player(){
+    if(on === true && simonTurn === false && PlayerTurn === false){
+        $btnb.addEventListener('click', () => {
+            $0 = 0;
+            if($0 === 0){
+                playerScore.push($0);
+                console.log(playerScore);
+                $btnb.classList.add('activated');
+                setTimeout(function (){
+                    $btnb.classList.remove('activated');
+                },1000);
+            };
+        });
+        $btng.addEventListener('click', () => {
+            $1 = 1;
+            if($1 === 1){
+                playerScore.push($1);
+                console.log(playerScore);
+                $btng.classList.add('activated');
+                setTimeout(function (){
+                    $btng.classList.remove('activated');
+                },1000);
+            };
+        });
+        $btnr.addEventListener('click', () => {
+            $2 = 2;
+            if($2 === 2){
+                playerScore.push($2);
+                console.log(playerScore);
+                $btnr.classList.add('activated');
+                setTimeout(function (){
+                    $btnr.classList.remove('activated');
+                },1000);
+            };
+        });
+        $btny.addEventListener('click', () => {
+            $3 = 3;
+            if($3 === 3){
+                playerScore.push($3);
+                console.log(playerScore);
+                $btny.classList.add('activated');
+                setTimeout(function (){
+                    $btny.classList.remove('activated');
+                },1000);
+            }
+        });
+    };
+    
+}
+function match(){
+    if(playerScore = simonOrder ){
+        PlayerTurn = false;
+        simonTurn = true;
+        if(simonTurn = true){
+            play();
+        }
+        console.log(PlayerTurn);
+        console.log(simonTurn);
+    };
+};
 function flash(){
     if(simon == 0){
             $btnb.classList.add('activated');
@@ -170,7 +194,6 @@ function flash(){
     }
 };
 
-
 function resetGame(){
     setTimeout(function() {
         $scorebx1.innerHTML = "--";
@@ -180,6 +203,7 @@ function resetGame(){
         on = false;
         if(on === false){
             console.clear();
+            playerScore = [];
             simon = [];
             simonOrder = [];
             simonTurn = true;
